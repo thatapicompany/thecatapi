62d8ead675dafb7cf28e04bd11d4ac4f1eba14ab
Fibi <charafbahria@gmail.com>
2022-09-21 21:21:35 +0100

    [ApiRequest] re-export defaults

---
+++ src/services/ApiRequest/index.ts
@@ -0,0 +1,3 @@
+export { default as ApiRequest } from "./ApiRequest";
+export { default as ApiRequestError } from "./ApiRequestError";
+export { default as ApiResponseError } from "./ApiResponseError";
