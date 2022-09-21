f707781b9b6df7860e08f5408f3545678923d256
Fibi <charafbahria@gmail.com>
2022-09-21 21:41:20 +0100

    [Images] add ImagesInterface

---
+++ src/endpoints/Images/ImagesInterface.ts
@@ -0,0 +1,25 @@
+export type Image = {
+  id: string;
+  width: number;
+  height: number;
+  url: string;
+  breeds?: Record<string, any>[];
+  categories?: Record<string, any>[];
+};
+
+export type GetImagesFilter = {
+    limit?: number,
+    page?: number,
+    order?: "ASC" | "DESC" | "RAND",
+    hasBreeds?: boolean,
+    breeds?: string[],
+    categories?: number[],
+    subId?: string,
+    mimeTypes?: ("jpg" | "png" | "gif")[],
+    format?: "json" | "src",
+    size?: "small" | "med" | "full",
+};
+
+export interface ImagesInterface {
+  getImages(filter?: GetImagesFilter): Promise<Image[]>;
+}
