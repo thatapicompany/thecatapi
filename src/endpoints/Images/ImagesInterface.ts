944415bb80e6585ced0d29227cedacff4a78000b
Fibi <charafbahria@gmail.com>
2022-09-25 18:26:25 +0100

    [ImagesInterface] add getImages

--- src/endpoints/Images/ImagesInterface.ts
+++ src/endpoints/Images/ImagesInterface.ts
@@ -34,9 +34,60 @@ export type UploadImageResponse = Omit<Image, "categories"> & {
   approved: boolean;
 };
 
+export type GetImagesFilter = SearchImagesFilter & {
+  originalFilename?: string;
+};
+
+type ImageVote = {
+  id: number;
+  value: number;
+};
+
+type ImageFavourite = {
+  id: number;
+};
+
+export type UserImageResponse = Image & {
+  breeds: Record<string, any>;
+  breed_ids: string | null;
+  original_filename: string;
+  sub_id: string | null;
+  created_at: Date;
+  vote?: ImageVote;
+  favourite?: ImageFavourite;
+};
+
+export type UserImage = Image & {
+  breeds: Record<string, any>;
+  breedId: string | null;
+  originalFilename: string;
+  subId: string | null;
+  createdAt: Date;
+  vote?: ImageVote;
+  favourite?: ImageFavourite;
+};
+
+export function mapUserImage(response: UserImageResponse): UserImage {
+  return {
+    id: response.id,
+    originalFilename: response.original_filename,
+    url: response.url,
+    breeds: response.breeds,
+    breedId: response.breed_ids ?? null,
+    categories: response.categories,
+    subId: response.sub_id ?? null,
+    height: response.height,
+    width: response.width,
+    createdAt: response.created_at,
+    favourite: response.favourite,
+    vote: response.vote,
+  };
+}
+
 export interface ImagesInterface {
   searchImages(filter?: SearchImagesFilter): Promise<Image[]>;
   getImage(id: string): Promise<Image>;
+  getImages(filter?: GetImagesFilter): Promise<UserImage[]>;
   getRandomImage(filter?: GetRandomImageFilter): Promise<Image | null>;
   uploadImage(
     image: File | stream.Readable,
