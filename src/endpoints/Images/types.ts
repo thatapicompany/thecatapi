e8097b5cd6a723b9588328505ed2274f52163cca
Fibi <charafbahria@gmail.com>
2022-09-26 19:08:43 +0100

    [Images/types] update uploadImage types

--- src/endpoints/Images/types.ts
+++ src/endpoints/Images/types.ts
@@ -55,8 +55,8 @@ type ImageFavourite = {
   id: number;
 };
 
-export type UserImageResponse = Image & {
-  breeds: Record<string, any>;
+export type UserImageResponse = Omit<Image, "breeds"> & {
+  breeds: Record<string, any>[];
   breed_ids: string | null;
   original_filename: string;
   sub_id: string | null;
@@ -65,8 +65,8 @@ export type UserImageResponse = Image & {
   favourite?: ImageFavourite;
 };
 
-export type UserImage = Image & {
-  breeds: Record<string, any>;
+export type UserImage = Omit<Image, "breeds"> & {
+  breeds: Record<string, any>[];
   breedId: string | null;
   originalFilename: string;
   subId: string | null;
