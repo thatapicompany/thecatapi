36935d3455a13cdfca2eb2312ac6c85de1e63657
Fibi <charafbahria@gmail.com>
2022-09-27 18:56:06 +0100

    [favourites.interface] add deleteFavourite

--- src/endpoints/favourites/favourites.interface.ts
+++ src/endpoints/favourites/favourites.interface.ts
@@ -1,7 +1,8 @@
-import { Favourite } from "./types";
+import { DeleteFavourite, Favourite } from "./types";
 
 export interface FavouritesInterface {
   getFavourites(subId?: string): Promise<Favourite[]>;
   getFavourite(id: number): Promise<Favourite>;
   addFavourite(imageId: string, subId?: string): Promise<Favourite>;
+  deleteFavourite(id: number): Promise<DeleteFavourite>;
 }
