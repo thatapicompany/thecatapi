aace6acd1742e68f61cd095b35dc97dff390fc16
Fibi <charafbahria@gmail.com>
2022-09-27 17:23:22 +0100

    [favourites.interface] add getFavourite

--- src/endpoints/favourites/favourites.interface.ts
+++ src/endpoints/favourites/favourites.interface.ts
@@ -2,4 +2,5 @@ import { Favourite } from "./types";
 
 export interface FavouritesInterface {
   getFavourites(subId?: string): Promise<Favourite[]>;
+  getFavourite(id: number): Promise<Favourite>;
 }
