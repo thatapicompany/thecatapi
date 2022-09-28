536dfa5ac00b3eec5a25ecc07f6f54c420786cd1
Fibi <charafbahria@gmail.com>
2022-09-28 10:00:22 +0100

    [votes.interface] add getVote

--- src/endpoints/votes/votes.interface.ts
+++ src/endpoints/votes/votes.interface.ts
@@ -1,5 +1,6 @@
-import { Vote } from "./types";
+import { GetVote, Vote } from "./types";
 
 export interface VotesInterface {
   getVotes(subId?: string): Promise<Vote[]>;
+  getVote(id: number): Promise<GetVote>;
 }
