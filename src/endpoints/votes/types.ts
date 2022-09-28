079240f7dd1d7e50296e1ba80e7138850d685cde
Fibi <charafbahria@gmail.com>
2022-09-28 10:46:18 +0100

    [votes/types] add addVote types

--- src/endpoints/votes/types.ts
+++ src/endpoints/votes/types.ts
@@ -25,8 +25,34 @@ export type Vote = {
 
 export type GetVoteResponse = VoteResponse & {
   user_id: string;
-}
+};
 
 export type GetVote = Vote & {
   userId: string;
+};
+
+export type AddVoteData = {
+  imageId: string;
+  subId?: string;
+  value: number;
+};
+
+export type AddVoteRequestData = {
+  image_id: string;
+  sub_id?: string;
+  value: number;
+};
+
+export type AddVoteResponse = {
+  id: number;
+  image_id: string;
+  sub_id?: string;
+  value: number;
+  country_code: string;
+  message: string;
+};
+
+export type AddVote = Omit<Vote, "createdAt" | "image" | "subId"> & {
+  subId?: string;
+  message: string;
 };
