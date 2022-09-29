f5bc83347193a4068582811bc658856ad6415f74
Fibi <charafbahria@gmail.com>
2022-09-29 11:02:26 +0100

    [votes.interface] add deleteVote

--- src/endpoints/votes/votes.interface.ts
+++ src/endpoints/votes/votes.interface.ts
@@ -1,7 +1,8 @@
-import { AddVote, AddVoteData, GetVote, Vote } from "./types";
+import { AddVote, AddVoteData, DeleteVote, GetVote, Vote } from "./types";
 
 export interface VotesInterface {
   getVotes(subId?: string): Promise<Vote[]>;
   getVote(id: number): Promise<GetVote>;
   addVote(data: AddVoteData): Promise<AddVote>;
+  deleteVote(id: number): Promise<DeleteVote>;
 }
