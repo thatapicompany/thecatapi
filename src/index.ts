a31cc34b9e39c3765ca6ae01d7ffbdc02664ba53
Fibi <charafbahria@gmail.com>
2022-10-26 12:26:18 +0100

    [index] export Breed

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { default as TheCatAPI } from "./thecatapi";
 export { ApiResponseError, ApiRequestError } from "./services/ApiRequest";
+export { Breed } from "./endpoints/images/breed.enum";
